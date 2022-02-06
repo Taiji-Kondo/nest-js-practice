c4b29b8d242a69d4454b385df02df56b51c7f9d6
taiji <k.taiji1108@gmail.com>
2022-02-06 18:06:34 +0900

    chore: コメントアウト削除

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -54,33 +54,4 @@ export class UsersService {
       where,
     });
   }
-  //
-  // create(createUserInput: CreateUserInput) {
-  //   const id = this.users.at(-1).id + 1
-  //   this.users.push({id, ...createUserInput})
-  //   return createUserInput
-  // }
-  //
-  // findAll() {
-  //   return this.users;
-  // }
-  //
-  // findOne(id: number) {
-  //   return this.users.find((user) => user.id === id);
-  // }
-  //
-  // update(id: number, updateUserInput: UpdateUserInput) {
-  //   const targetUser = this.findOne(id);
-  //
-  //   targetUser.name = updateUserInput.name;
-  //
-  //   return targetUser;
-  // }
-  //
-  // remove(id: number) {
-  //   const targetUser = this.findOne(id);
-  //   this.users = this.users.filter((user) => user.id !== id);
-  //
-  //   return targetUser;
-  // }
 }
